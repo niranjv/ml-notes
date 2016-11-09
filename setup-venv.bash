13795a1dac3e9e3b38cc7f3e7f7f9e27be51ef25
Niranjan <niranjanv+github@gmail.com>
2016-11-08 23:38:40 -0800

    add info abt frameworkpython to make matplotlib work from Terminal in virtualenv

--- setup-venv.bash
+++ setup-venv.bash
@@ -5,6 +5,27 @@ mkdir python
 cd python/
 virtualenv venv
 
+# ----------
+
+# now add a file called venv/bin/frameworkpython to make matplotlib work from Terminal in virtualenv:
+# for more info, see http://matplotlib.org/faq/virtualenv_faq.html#pythonhome-script
+
+# #!/bin/bash
+
+# # what real Python executable to use
+# PYVER=2.7
+# PATHTOPYTHON=/usr/bin/
+# PYTHON=${PATHTOPYTHON}python${PYVER}
+
+# # find the root of the virtualenv, it should be the parent of the dir this script is in
+# ENV=`$PYTHON -c "import os; print(os.path.abspath(os.path.join(os.path.dirname(\"$0\"), '..')))"`
+
+# # now run Python with the virtualenv set as Python's HOME
+# export PYTHONHOME=$ENV
+# exec $PYTHON "$@"
+
+# ----------
+
 source venv/bin/activate
 
 pip install numpy scipy sympy pandas nose ipython jupyter matplotlib seaborn
@@ -14,6 +35,3 @@ pip install sklearn
 # proceed in notebook
 
 deactivate
-
-# See also venv/bin/frameworkpython
-# to make matplotlib work from Terminal in virtualenv
